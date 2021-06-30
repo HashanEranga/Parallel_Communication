b74d6678b24feeba8454497d3a0cc7658b3fd0e8
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-06-30 12:03:39 +0530

    debug(): fix issues in the submission

--- Part2/code.c
+++ Part2/code.c
@@ -18,34 +18,34 @@ void main(){
 
     while(1){
         // lit the 0 value in the seven segment
-        outb(0b01111111, DATA_PORT);
+        outb(0b11000000, DATA_PORT);
         __delay_ms(1000);
         // lit the 1 value in the seven segment
-        outb(0b00001101, DATA_PORT);
+        outb(0b11111001, DATA_PORT);
         __delay_ms(1000);
         // lit the 2 value in the seven segment
-        outb(0b10110111, DATA_PORT);
+        outb(0b10100100, DATA_PORT);
         __delay_ms(1000);
         // lit the 3 value in the seven segment
-        outb(0b10011111, DATA_PORT);
+        outb(0b10110000, DATA_PORT);
         __delay_ms(1000);
         // lit the 4 value in the seven segment
-        outb(0b11001101, DATA_PORT);
+        outb(0b10011001, DATA_PORT);
         __delay_ms(1000);
         // lit the 5 value in the seven segment
-        outb(0b11011011, DATA_PORT);
+        outb(0b10010010, DATA_PORT);
         __delay_ms(1000);
         // lit the 6 value in the seven segment
-        outb(0b11111011, DATA_PORT);
+        outb(0b10000010, DATA_PORT);
         __delay_ms(1000);
         // lit the 7 value in the seven segment
-        outb(0b00001111, DATA_PORT);
+        outb(0b01111000, DATA_PORT);
         __delay_ms(1000);
         // lit the 8 value in the seven segment
-        outb(0b11111111, DATA_PORT);
+        outb(0b10000000, DATA_PORT);
         __delay_ms(1000);
         // lit the 9 value in the seven segment
-        outb(0b11011111, DATA_PORT);
+        outb(0b10011000, DATA_PORT);
         __delay_ms(1000);
     }
     
